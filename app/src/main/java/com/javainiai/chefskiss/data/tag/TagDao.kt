5f60909580d0a5de4e889d64370f1e4e5276a888
Laizymario <benasbulk@gmail.com>
2024-03-01 16:51:00 +0200

    Fixed mistakes

--- app/src/main/java/com/javainiai/chefskiss/data/tag/TagDao.kt
+++ app/src/main/java/com/javainiai/chefskiss/data/tag/TagDao.kt
@@ -1,12 +1,13 @@
 package com.javainiai.chefskiss.data.tag
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
-
+@Dao
 interface TagDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(item: Tag)
