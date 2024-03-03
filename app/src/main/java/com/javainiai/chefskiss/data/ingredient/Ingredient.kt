90ebaf9795a06cb5cf9958b92adb43ae3e88fe4d
dov-vai <160327869+dov-vai@users.noreply.github.com>
2024-03-03 21:28:28 +0200

    Add foreign key for one to many relation, and change size from int to float

--- app/src/main/java/com/javainiai/chefskiss/data/ingredient/Ingredient.kt
+++ app/src/main/java/com/javainiai/chefskiss/data/ingredient/Ingredient.kt
@@ -1,15 +1,29 @@
 package com.javainiai.chefskiss.data.ingredient
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.javainiai.chefskiss.data.recipe.Recipe
 
-@Entity(tableName = "ingredients")
+@Entity(
+    tableName = "ingredients",
+    foreignKeys = [
+        ForeignKey(
+            entity = Recipe::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("recipeId"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Ingredient(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
+    @ColumnInfo(index = true)
     val recipeId: Int,
     val name: String,
-    val size: Int,
+    val size: Float,
     val unit: String,
     val allergen: Boolean
 )
