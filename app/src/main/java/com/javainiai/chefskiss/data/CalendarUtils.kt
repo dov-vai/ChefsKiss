d4a2082d80e5a9c6e9d3f31ad100bdd74ef935bf
dov-vai <160327869+dov-vai@users.noreply.github.com>
2024-03-31 16:34:45 +0300

    Create CalendarUtils for easier work with dates

---
+++ app/src/main/java/com/javainiai/chefskiss/data/CalendarUtils.kt
@@ -0,0 +1,36 @@
+package com.javainiai.chefskiss.data
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+
+object CalendarUtils {
+    fun getStartOfWeek(): Date {
+        val calendar = Calendar.getInstance()
+        calendar.firstDayOfWeek = Calendar.MONDAY
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+        return calendar.time
+    }
+
+    fun getEndOfWeek(): Date {
+        val calendar = Calendar.getInstance()
+        calendar.firstDayOfWeek = Calendar.MONDAY
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
+        calendar.add(Calendar.DAY_OF_WEEK, 6)
+        return calendar.time
+    }
+
+
+    fun getCurrentDate(): Date {
+        val calendar = Calendar.getInstance()
+        return calendar.time
+    }
+
+    fun datePlusOffset(date: Date, dayOffset: Int): Date {
+        val calendar = Calendar.getInstance()
+        calendar.setTime(date)
+        calendar.add(Calendar.DATE, dayOffset)
+        return calendar.time
+    }
+}
