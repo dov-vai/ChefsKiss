321eb7e54e8807d9268e7f3b037aed694ab135d6
dov-vai <160327869+dov-vai@users.noreply.github.com>
2024-03-31 16:46:20 +0300

    Remove unused imports

--- app/src/main/java/com/javainiai/chefskiss/data/CalendarUtils.kt
+++ app/src/main/java/com/javainiai/chefskiss/data/CalendarUtils.kt
@@ -1,9 +1,7 @@
 package com.javainiai.chefskiss.data
 
-import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
-import java.util.Locale
 
 object CalendarUtils {
     fun getStartOfWeek(): Date {
