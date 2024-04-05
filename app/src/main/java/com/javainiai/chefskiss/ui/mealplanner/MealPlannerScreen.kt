9294ac8bcee144adfce847693a07ba075eacda5d
dov-vai <160327869+dov-vai@users.noreply.github.com>
2024-04-05 20:43:28 +0300

    Load planner meals from database

--- app/src/main/java/com/javainiai/chefskiss/ui/mealplanner/MealPlannerScreen.kt
+++ app/src/main/java/com/javainiai/chefskiss/ui/mealplanner/MealPlannerScreen.kt
@@ -3,6 +3,7 @@ package com.javainiai.chefskiss.ui.mealplanner
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,22 +11,31 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.javainiai.chefskiss.data.CalendarUtils
+import com.javainiai.chefskiss.data.recipe.PlannerRecipeWithRecipe
 import com.javainiai.chefskiss.ui.AppViewModelProvider
 import com.javainiai.chefskiss.ui.navigation.NavigationDestination
 import java.text.SimpleDateFormat
@@ -38,9 +48,11 @@ object MealPlannerDestination : NavigationDestination {
 @Composable
 fun MealPlannerScreen(
     navigateBack: () -> Unit,
-    viewModel: MealPlannerViewModel = viewModel(factory = AppViewModelProvider.Factory)
+    viewModel: MealPlannerViewModel = viewModel(factory = AppViewModelProvider.Factory),
+    navigateTo: (String) -> Unit,
 ) {
     val uiState by viewModel.uiState.collectAsState()
+    val plannerRecipes by viewModel.plannerRecipes.collectAsState()
     Scaffold(topBar = {
         MealPlannerTopBar(
             title = uiState.title,
@@ -53,16 +65,43 @@ fun MealPlannerScreen(
             repeat(7) {
                 val date = CalendarUtils.datePlusOffset(uiState.startOfWeek, it)
                 val title = SimpleDateFormat("EEEE, dd MMM", Locale.getDefault()).format(date)
+                val fullDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date)
                 item {
+                    var opened by remember { mutableStateOf(date == uiState.currentDate) }
                     WeekdayCard(
                         title = title,
-                        onEdit = {},
+                        onEdit = { navigateTo("${PlannerEditDestination.route}/$fullDate") },
+                        cardColor = if (date == uiState.currentDate) MaterialTheme.colorScheme.secondary else CardDefaults.cardColors().containerColor,
+                        opened = opened,
+                        onOpen = { opened = !opened },
                         modifier = Modifier.padding(8.dp)
                     ) {
-                        // load entries from database
+                        plannerRecipes[fullDate]?.forEach { recipe ->
+                            PlannerRecipeCard(
+                                recipe = recipe,
+                                cardColor = if (date == uiState.currentDate) MaterialTheme.colorScheme.secondary else CardDefaults.cardColors().containerColor,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun PlannerRecipeCard(
+    recipe: PlannerRecipeWithRecipe,
+    cardColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Card(modifier = modifier, colors = CardDefaults.cardColors(containerColor = cardColor)) {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
+            Text(text = recipe.plannerRecipe.type.title)
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = recipe.recipe.title)
+            Spacer(modifier = Modifier.weight(1f))
         }
     }
 }
@@ -111,20 +150,35 @@ fun MealPlannerTopBar(
 fun WeekdayCard(
     title: String,
     onEdit: () -> Unit,
+    cardColor: Color,
+    opened: Boolean,
+    onOpen: () -> Unit,
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
-    Card(modifier = modifier, shape = RoundedCornerShape(32.dp)) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(32.dp),
+        colors = CardDefaults.cardColors(containerColor = cardColor)
+    ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Text(text = title)
                 Spacer(modifier = Modifier.weight(1f))
+                IconButton(onClick = onOpen) {
+                    Icon(
+                        imageVector = if (opened) Icons.Default.ArrowDropUp else Icons.Default.ArrowDropDown,
+                        contentDescription = "Open/Close card"
+                    )
+                }
                 IconButton(onClick = onEdit) {
                     Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
                 }
             }
-            Column {
-                content()
+            if (opened) {
+                Column {
+                    content()
+                }
             }
         }
     }
--- app/src/main/java/com/javainiai/chefskiss/ui/mealplanner/MealPlannerViewModel.kt
+++ app/src/main/java/com/javainiai/chefskiss/ui/mealplanner/MealPlannerViewModel.kt
@@ -1,10 +1,16 @@
 package com.javainiai.chefskiss.ui.mealplanner
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.javainiai.chefskiss.data.CalendarUtils
+import com.javainiai.chefskiss.data.recipe.PlannerRecipeWithRecipe
 import com.javainiai.chefskiss.data.recipe.RecipesRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -25,6 +31,25 @@ class MealPlannerViewModel(recipesRepository: RecipesRepository) : ViewModel() {
     )
     val uiState = _uiState.asStateFlow()
 
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+    val plannerRecipes: StateFlow<Map<String, List<PlannerRecipeWithRecipe>>> =
+        recipesRepository
+            .getPlannerRecipesWithRecipes(
+                (0..6).map {
+                    val date = CalendarUtils.datePlusOffset(CalendarUtils.getStartOfWeek(), it)
+                    dateFormat.format(date)
+                }
+            )
+            .map {
+                it.groupBy { it.plannerRecipe.date }
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = mapOf()
+            )
+
     private fun titleFormat(): String {
         val start = CalendarUtils.getStartOfWeek()
         val end = CalendarUtils.getEndOfWeek()
@@ -38,5 +63,9 @@ class MealPlannerViewModel(recipesRepository: RecipesRepository) : ViewModel() {
                 current.time
             )
         }"
+    }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
     }
 }
